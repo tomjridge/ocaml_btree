fd1c532150e5d442caa6769a06074e70bbaee6b2
Tom Ridge <tom.j.ridge@googlemail.co.uk>
2015-06-21 15:39:02 +0100

    +

--- .nix/default.nix
+++ .nix/default.nix
@@ -3,6 +3,7 @@ let
     pkgs = import <nixpkgs> {};
     stdenv = pkgs.stdenv;
     fetchgit = pkgs.fetchgit;
+    perl = pkgs.perl;
     isabelle = import ./isabelle { };
     lem = import ./lem { };
 in stdenv.mkDerivation {
@@ -11,27 +12,35 @@ in stdenv.mkDerivation {
     lem = lem;
     isabelle = isabelle;
   
-    src = fetchgit {
-      url = https://github.com/tomjridge/p3.git;
-      rev = "0f6c732";
-      sha256 = "5ea4a2486876b2bd382dcdada57593233ac6b1e681539e72d5e144e630aaf1ff";
-    };
-#    src = ./.;  
-    buildInputs = [ isabelle lem ]; 
+    src = lem;  
+    buildInputs = [ perl isabelle lem ]; 
   
     configurePhase = "true"; 	# Skip configure
     
+    # curPhase=buildPhase
+    # export out=/tmp
+    # eval "${!curPhase:-$curPhase}"
     buildPhase = ''
-      echo lemenv! execute the following using buildPhase() from nix-shell
+      echo lemenv execute the following using buildPhase from nix-shell
       cd ${lem}/lem
+      echo pwd is $PWD
+      export USER_HOME=$out
+      echo USER_HOME: $USER_HOME
       isabelle build -d isabelle-lib -b LEM
       '';
   
-    installPhase = "false"; # don't want to install
+    installPhase = "true"; # don't want to install
 
 
 shellHook = ''
+    export LEMPATH=${lem}/lem
     export PATH=$PATH:${lem}/lem
     export LEMLIB=${lem}/lem/library
+    #export ISABELLE_LOGIC=LEM
+    curPhase=buildPhase
+    export out=/tmp/isa
+    
   '';
+    #eval "${!curPhase:-$curPhase}"
+
 }
--- .nix/lem/default.nix
+++ .nix/lem/default.nix
@@ -22,7 +22,7 @@ in stdenv.mkDerivation {
     configurePhase = "true"; 	# Skip configure
     
     buildPhase = ''
-      export ISABELLE_JDK_HOME=${java}
+      # export ISABELLE_JDK_HOME=${java}
       export LD_LIBRARY_PATH=`pwd`/ocaml-lib/dependencies/zarith:$LD_LIBRARY_PATH  # complete hack - need to find dllzarith.so
       echo 'let v="2739f6a"' >src/version.ml  # complete hack - the source code isn't a git repo after fetchgit
       echo "!!!"
@@ -31,10 +31,10 @@ in stdenv.mkDerivation {
       echo "!!!"
       make ocaml-libs
 
-      mkdir -p $out/lem/.isabelle  # after build, need to copy these images to local .isabelle
-      export USER_HOME=$out/lem
+      #mkdir -p $out/lem/.isabelle  # after build, need to copy these images to local .isabelle
+      #export USER_HOME=$out/lem
       # make isa-libs
-      # isabelle build -d isabelle-lib -b LEM # do this in the lem source dir
+      #isabelle build -d isabelle-lib -b LEM # do this in the lem source dir
 
       # sudo chmod u+w ~/.isabelle/Isabelle2014/heaps/polyml-5.5.2_x86-linux/*
       # cp .isabelle/Isabelle2014/heaps/polyml-5.5.2_x86-linux/* ~/.isabelle/Isabelle2014/heaps/polyml-5.5.2_x86-linux/
@@ -56,6 +56,6 @@ mkdir -p $out/lem && cp -R -L * $out/lem
 ''; # so we can inspect the result
     # note that we need to export LEM_LIBRARY_PATH=<absolute path to lem directory>/library before invoking lem
 
-    createFindlibDestdir = true;
+#    createFindlibDestdir = true;
 
 }
