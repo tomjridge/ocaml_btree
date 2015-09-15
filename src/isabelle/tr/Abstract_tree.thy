c7498155b5b10b24a209730b7e540ab75dd56d57
Tom Ridge <tom.j.ridge@googlemail.com>
2015-09-15 10:52:39 +0100

    +

--- src/isabelle/tr/Abstract_tree.thy
+++ src/isabelle/tr/Abstract_tree.thy
@@ -4,8 +4,20 @@ imports
  	 Main
 begin
 
+(* Quickcheck_Examples/Completeness.thy - should be in Main? simpler defn here*)
+definition is_Some :: "'a option => bool" where
+  "is_Some x == x ~= None"
+
+primrec dest_Some (* :: "'a option => 'a" *) where 
+  "dest_Some (Some x) = x"
+  | "dest_Some None = undefined"
+
+
 definition arb :: "'a" where
   "arb == SOME x. True"  (* where is this in lib? *)
+
+definition rev_apply :: "'a => ('a => 'b) => 'b" (infixl "|>" 100) where
+  "rev_apply x f = f x"
 
 
 (* type vars: 'bs 'k 'r 'v *)
@@ -14,15 +26,23 @@ datatype 'bs page = Page 'bs (* bytes *)
 
 datatype 'r page_ref = Page_ref 'r
 
+definition dest_page_ref :: "'r page_ref => 'r" where
+  "dest_page_ref r0 == (case r0 of Page_ref r => r)"
+
 datatype ('r,'bs) store = Store "('r ~=> 'bs page)"  (* different to paper: we store actual bytes *)
 
+definition dest_store :: "('r,'bs) store => ('r ~=> 'bs page)" where
+  "dest_store s0 == (case s0 of Store f => f)"
+
+definition ref_to_page :: "('r,'bs) store => 'r page_ref => 'bs page option" where
+  "ref_to_page s0 r0 == (s0|>dest_store) (r0|>dest_page_ref)"
 
 datatype 'k key = Key 'k
 
 record ('r,'k) node_frame = 
   nf_n :: "nat"
   nf_ks :: "nat => 'k key"
-  nf_ps :: "nat => 'r page_ref"
+  nf_rs :: "nat => 'r page_ref"
 
 record ('k,'v) leaf_frame = 
   lf_kvs :: "('k * 'v) list" (* slightly different to paper - we store ks in tree *) 
@@ -30,16 +50,124 @@ record ('k,'v) leaf_frame =
 datatype ('r,'k,'v) frame = Frm_I "('r,'k) node_frame" | Frm_L "('k,'v) leaf_frame"
 
 (* interpretation of pages *)
-datatype ('bs,'k,'r,'v) page_to_frame_0 = P2f0 "'bs page => ('k,'v) leaf_frame"
-datatype ('bs,'k,'r,'v) page_to_frame_suc = P2fs "'bs page => ('r,'k) node_frame"
+datatype ('bs,'k,'r,'v) page_to_frame = P2f "'bs page => ('r,'k,'v) frame"  (* note that this forces that the page internally stores its type; this is not necessary, but is used by step_find *)
+
+definition dest_p2f :: "('bs,'k,'r,'v) page_to_frame => 'bs page => ('r,'k,'v) frame" where
+  "dest_p2f x = (case x of P2f f => f)"
+
 
 (* to convert a page_ref to a frame, lookup the page option and use the above *)
 
 
 datatype ('k,'v) tree = Tr_nd "(nat * (nat => 'k key) * (nat => ('k,'v) tree))" | Tr_lf "('k * 'v) list"
 
-definition page_ref_to_tree :: "('r,'bs) store => 'r page_ref => nat => ('k,'v) tree option" where
-  "page_ref_to_tree s0 r0 n0 == arb::('k,'v)tree option"
+function tree_to_kvs :: "('k,'v) tree => ('k*'v) list" where
+  "tree_to_kvs (Tr_lf(kvs)) = kvs"
+  | "tree_to_kvs (Tr_nd(n,ks,ts)) = ([0..<n] |> (List.map ts) |> (List.map tree_to_kvs) |> List.concat)"
+apply (metis PairE tree.exhaust)
+apply (metis tree.inject(2))
+apply (metis tree.distinct(2))
+by (metis prod.sel(1) prod.sel(2) tree.inject(1))
+
+record  ('bs,'k,'r,'v) ctxt =
+  ctxt_p2f :: "('bs,'k,'r,'v) page_to_frame"
+
+(* from this point, we don't duplicate 0/Suc defns, to minimize # of defns *)
+definition page_ref_to_frame :: "('bs,'k,'r,'v) ctxt => ('r,'bs) store =>  'r page_ref => ('r,'k,'v) frame option" where
+  "page_ref_to_frame c0 s0 r0 == (
+    case ref_to_page s0 r0 of
+    None => None
+    | Some p => (Some( (c0|>ctxt_p2f|>dest_p2f) p))
+)"
+
+
+(* NB this has an explicit n argument, whereas wfness gives us that we can just use page_ref_to_frame *)
+fun page_ref_to_tree :: "('bs,'k,'r,'v) ctxt =>  ('r,'bs) store => 'r page_ref => nat => ('k,'v) tree option" where
+  "page_ref_to_tree c0 s0 r0 0 = (
+      case page_ref_to_frame c0 s0 r0 of 
+      None => None 
+      | Some frm => (
+        case frm of 
+        Frm_L(lf) => (Some(Tr_lf (lf|>lf_kvs)))
+        | _ => arb  (* impossible *)))"
+  | "page_ref_to_tree c0 s0 r0 (Suc n') = (
+    let n0 = (Suc n') in
+      case page_ref_to_frame c0 s0 r0 of
+      None => None
+      |  Some frm => (
+        case frm of 
+        Frm_I(nf) => (
+          let n = (nf|>nf_n) in
+          let ks = (nf|>nf_ks) in
+          let rs = (nf|>nf_rs) :: (nat => 'r page_ref) in
+          let f0 = (% r. page_ref_to_tree c0 s0 r n') :: ('r page_ref => ('k,'v) tree option) in 
+          case (! (m::nat). m < n --> m |> rs |> f0 |> is_Some)  of
+          True => (Some(Tr_nd(n,ks,(% (m::nat). m |> rs |> f0 |> dest_Some))))
+          | False => None)
+        | _ => arb  (* impossible *)))"
+
+definition page_ref_to_kvs ::  "('bs,'k,'r,'v) ctxt =>  ('r,'bs) store => 'r page_ref => nat => ('k*'v) list option" where
+  "page_ref_to_kvs c0 s0 r0 n0 == (
+  (page_ref_to_tree c0 s0 r0 n0)
+  |> (% x. case x of
+    None => None
+    | Some t => Some(tree_to_kvs t)))"
+
+(* find *)
+record ('bs,'k,'r,'v) find_state_l =
+  fsl_k :: "'k key"
+  fsl_r :: "'r page_ref"
+(*   fnd0_s :: "('r,'bs) store" *)
+
+record ('bs,'k,'r,'v) find_state_r =
+  fsr_r :: "'r page_ref"
+  fsr_v :: "'v option"
+(*  fnd1_s :: "('r,'bs) store" *)
+
+datatype ('bs,'k,'r,'v) find_state = Fs_l "('bs,'k,'r,'v) find_state_l" | Fs_r "('bs,'k,'r,'v) find_state_r"
+
+datatype ('bs,'k,'r,'v) key_to_ref = Key_to_ref "('r,'k) node_frame => 'k key => 'r page_ref" 
+datatype ('bs,'k,'r,'v) key_to_v = Key_to_v "('k,'v) leaf_frame => 'k key => 'v option"  (* may be no such v *)
+
+definition dest_key_to_ref :: "('bs,'k,'r,'v) key_to_ref => ('r,'k) node_frame => 'k key => 'r page_ref" where
+  "dest_key_to_ref k2r == (case k2r of Key_to_ref f => f)"
+
+definition dest_key_to_v :: "('bs,'k,'r,'v) key_to_v => ('k,'v) leaf_frame => 'k key => 'v option" where
+  "dest_key_to_v k2v == (case k2v of Key_to_v f => f)"
+
+
+record  ('bs,'k,'r,'v) ctxt1 =  "('bs,'k,'r,'v) ctxt" +
+  key_to_ref :: "('bs,'k,'r,'v) key_to_ref"
+  key_to_v :: "('bs,'k,'r,'v) key_to_v"
+
+
+
+
+definition fs_step :: "('bs,'k,'r,'v) ctxt1
+  => (('r,'bs) store * ('bs,'k,'r,'v) find_state) 
+  => (('r,'bs) store * ('bs,'k,'r,'v) find_state) option" where
+  "fs_step ctxt1 s0fs0 == (
+  let (s0,fs0) = s0fs0 in
+  case fs0 of
+  Fs_l fsl => (
+    let r0 = (fsl|>fsl_r) in
+    let k0 = (fsl|>fsl_k) in
+    case (page_ref_to_frame (ctxt.truncate ctxt1) s0 r0) of 
+    None => None
+    | Some frm => (
+      case frm of 
+      Frm_I nf => (
+        let k2r = ((ctxt1|>key_to_ref)|>dest_key_to_ref) in
+        let r' = k2r nf k0 in
+        Some(s0, Fs_l (fsl (| fsl_r := r' |))))
+      | Frm_L lf => (
+        let k2v = ((ctxt1|>key_to_v)|>dest_key_to_v) in
+        let v = k2v lf k0 in
+        Some(s0, Fs_r (| fsr_r = r0, fsr_v = v |)))))
+  | Fs_r fsr => None)"
+
+
+
 
 (*
 locale l0 = 
